384215beb3dfe5db817c9277d9b2131880f02b25
Matthew Wix <mat_twix@hotmail.com>
2019-11-22 09:43:43 -0500

    Update settings.py

--- GetYouFit/GetYouFit/settings.py
+++ GetYouFit/GetYouFit/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'GetYouFit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -77,9 +77,7 @@ DATABASES = {
 	'PORT': '',
     }
 }
-"""
 
-DATABASES = {}
 DATABASES['default'].update(dj_database_url.config())
 
 
