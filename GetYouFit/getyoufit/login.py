7c779549e273006f8eec11e937e724d02dd7bfb4
Matthew Wix <mat_twix@hotmail.com>
2019-11-22 12:51:15 -0500

    Update login.py

--- GetYouFit/getyoufit/login.py
+++ GetYouFit/getyoufit/login.py
@@ -19,7 +19,7 @@ def login(info):
 
 		cursor = conn.cursor()
 	
-		cursor.execute("CREATE TABLE users VALUES(username VARCHAR(50) PRIMARY KEY, password VARCHAR(100) NOT NULL, age INT, height FLOAT, weight FLOAT, gender CHAR(1))")
+		cursor.execute("CREATE TABLE users (username VARCHAR(50) PRIMARY KEY, pword VARCHAR(100) NOT NULL, age INT, height FLOAT, weight FLOAT, gender CHAR(1))")
 		returnString= "Table created"
 		"""
 		cursor.execute("SELECT * FROM users WHERE username = '" + username + "'")
