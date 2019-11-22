94968a0da4727c6d5b4629f6d57c082cd0407bdd
Matthew Wix <mat_twix@hotmail.com>
2019-11-22 13:24:42 -0500

    Update init.js

--- GetYouFit/getyoufit/static/scripts/init.js
+++ GetYouFit/getyoufit/static/scripts/init.js
@@ -18,11 +18,10 @@ function updateForm(elem){
 	if(counter > 1){
 		document.getElementById("login-error-message").innerHTML = "The character ';' is not allowed.";
 		//elem.value = elem.value.substr(0, elem.value.length-1);
-	}
-	//else{
-		//document.getElementById("login-error-message").innerHTML = "";
+	}else{
+		document.getElementById("login-error-message").innerHTML = "";
 	//	document.getElementById('id_textarea').textContent = document.getElementById('username').value + ";" + document.getElementById('password').value;
-	//}
+	}
 }
 
 function validateLogin(){
